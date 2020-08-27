9b6a10d9b996557d25ae44610daf7ff398e42a6f
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-08-27 10:47:54 +0900

    Add new lines

--- lib/materialize-sass/helpers.rb
+++ lib/materialize-sass/helpers.rb
@@ -5,9 +5,11 @@ module Materialize
       valid_toast = ["error", "message", "success", "warning"]
       js_toast = ""
       queue = 0
+
       flash.each do |type, message|
         next if message.blank?
         type = type.to_s
+
         case type
         when "alert"
           type = "error"
@@ -16,12 +18,16 @@ module Materialize
         when "info"
           type = "message"
         end
+
         next unless valid_toast.include?(type)
+
         Array(message).each do |msg|
           js_toast << "setTimeout(function(){ M.toast({html: '#{j(msg)}'}) }, #{queue});"
         end
+
         queue += 100
       end
+
       flash.clear
       return js_toast.html_safe()
     end
